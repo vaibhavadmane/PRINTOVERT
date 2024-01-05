912d5ec7ec2c24af3c59e4ee3f21b5ad84e3974b
Mayank <mayank0real0world@gmail.com>
2024-01-05 12:31:53 +0530

    add profile edit folder

--- FRONTEND/src/components/Editprofie/Editprofile.jsx
+++ FRONTEND/src/components/Editprofie/Editprofile.jsx
@@ -3,23 +3,62 @@ import React from 'react'
 function Editprofile() {
   return (
     
-    <div className='bg-blue-200 w-full h-180'> 
+    <div className='bg-blue-200 w-full h-180 '>
 
- 
+    <div>
+  <h1 className=' font-bold mt-8 ml-7  text-blufont-cerebriSans text-3xle-900 co text-4xl'>Personal Detail</h1>
+  <p className='ml-7 mt-1'>Dont forget to save your changes </p>
+  </div>
+
+<div className='flex justify-between w-[90%] mt-16 font-semibold '>
+  <div className='ml-7 text-xl'>Name</div>
+  <div className='w-[50%] flex justify-between'><input className=' rounded-lg p-2 w-[45%]' type="text" placeholder='Enter the detail' />
+   <input className=' rounded-lg  p-2  w-[45%]' type="text" placeholder='Enter the detail' /></div>
+</div>
 
 
-  <div className=' w-screen  mt-10 flex'>
-
-  {/* <div className='w-96 bg-red-700 h-96 '><img className=' w-96 h-72' src="https://www.printondemandindia.in/_next/image?url=%2Fassets%2Fimages%2Fcanvas%2FTShirt2%2FBlack%2FBlack%20(1).png&w=1080&q=75" alt="" /> </div> */}
-    <div className='text-4xl text-red-600 ml-64 mt-40'>WE ARE WORKING ON THIS UI </div>
-    </div>
-
-
+<div className='flex justify-between w-[90%] mt-16 font-semibold '>
+  <div className='ml-7 text-xl'>Email-ID</div>
+  <div className='w-[50%] flex justify-between'><input className=' rounded-lg p-2 w-[45%]' type="text" placeholder='Enter the detail' />
+   <input className=' rounded-lg  p-2  w-[45%]' type="text" placeholder='Enter the detail' /></div>
+</div>
 
 
+<div className='flex justify-between w-[90%] mt-16 font-semibold '>
+  <div className='ml-7 text-xl'>Phone Number</div>
+  <div className='w-[50%] flex justify-between'><input className=' rounded-lg p-2 w-[100%]' type="text" placeholder='Enter the detail' /></div>
+</div>
 
-    </div>
 
+<div className='flex justify-between w-[90%] mt-16 font-semibold '>
+  <div className='ml-7 text-xl'>Password</div>
+  <div className='w-[80%] flex justify-between'>
+    <input className=' rounded-lg p-2 w-[45%] mr-5' type="text" placeholder='Enter current password' />
+   <input className=' rounded-lg  p-2  w-[45%] mr-5' type="text" placeholder='Enter new password' />
+   <input className=' rounded-lg  p-2  w-[45%]' type="text" placeholder='Confirm Password' /></div>
+</div>
+
+
+
+
+<div className='flex w-full ml-[77%] mt-3'>
+  <input  className='mr-2'  type="checkbox" />
+  <p>Show Password</p>
+</div>
+
+
+<div className='flex   w-[90%] mt-16 font-bold '>
+  <div className='ml-7 mr-10 text-2xl'>Addresses</div>
+  <button className='rounded-2xl w-28 bg-blue-600'>Add New</button>
+  </div>
+
+
+
+
+
+
+
+  </div>
   )
 }
 
--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Orders from './components/Orders/Orders.jsx'
